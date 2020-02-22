7774c1d81a65edae1fd1a4ec1189b5103922a14b
fexd12 <felipe.demelo@hotmail.com>
2020-02-21 23:58:18 -0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 
-import tempo from './tempo';
+import tempo from './api/tempo';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/tempo',tempo);
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
 
 app.listen(3000, () =>
     console.log('Servidor rodando na porta 3000'),
