c89f967f2de4fd7ccaf7f28af238c8ef938bd97a
fexd12 <felipe.demelo@hotmail.com>
2020-04-05 02:29:16 -0300

    adicionado tela de salas

---
+++ frontend/CoPilot/src/components/views/Salas.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div class="container">
+      <b-button class="btn btn-primary" v-b-modal.criaSala>
+        <span>Nova Sala</span>
+      </b-button>
+    </div>
+
+    <b-modal
+      id="criaSala"
+      title="Nova Sala"
+      ok-title="Salvar"
+      cancel-title="Cancelar"
+      @show="beforeSala"
+      @ok="saveSala"
+    >
+      <SalasForm v-model="ativoAtual" />
+    </b-modal>
+
+    <main role="main" class="container">
+      <div class="my-3 p-3 bg-white rounded shadow-sm">
+        <div class="media text-muted pt-3">
+          <div class="container">
+            <!-- No data message -->
+            <!-- Sala list -->
+
+            <div class="info-box-text">
+              <b-table
+                class="table table-bordered dataTable"
+                hover
+                fixed
+                head-variant="light"
+                :items="ativos"
+                :fields="fields"
+              ></b-table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import SalasForm from "../widgets/FormSalas";
+export default {
+  components: {
+    SalasForm
+  },
+  data: () => {
+    return {
+      ativoAtual: {
+        id: "",
+        nome: "",
+        quantidade: ""
+      },
+      ativos: [],
+      fields: [
+        {
+          key: "nome",
+          label: "nome"
+        },
+        {
+          key: "quantidade",
+          label: "quantidade"
+        }
+      ]
+    };
+  },
+  methods: {
+    async carregaTabela() {
+      this.ativos.splice(0, this.ativos.length);
+      let dados = await axios.get("http://localhost:3000/salas/", {});
+      this.ativos.push(...dados.data);
+    },
+    beforeSala() {
+      this.ativoAtual.nome = "";
+      this.ativoAtual.quantidade = "";
+    },
+    async saveSala() {
+      let payload = {
+        nome: this.ativoAtual.nome,
+        quantidade: this.ativoAtual.quantidade
+      };
+      try {
+        await axios.post("http://localhost:3000/salas/", payload);
+        await this.carregaTabela();
+      } catch (err) {
+        alert("erro ao inserir");
+      }
+    }
+  },
+  async mounted(){
+    await this.carregaTabela();
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/CoPilot/src/components/widgets/FormSalas.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-form>
+      <b-form-group id="input-group-1" label="Nome da Sala:" label-for="input-1">
+        <b-form-input v-model="content.nome" id="input-1" @input="handleInput" required />
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Quantidade de Cadeiras:" label-for="input-2">
+        <b-form-input v-model="content.quantidade" id="input-2" @input="handleInput" required />
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SalasForm",
+  props: ["value"],
+  data() {
+    return {
+      content: {
+        id: this.value.id,
+        nome: this.value.nome,
+        quantidade: this.value.quantidade
+      }
+    };
+  },
+  methods: {
+    handleInput() {
+      let retorno = {
+        id: this.content.id,
+        nome: this.content.nome,
+        quantidade: this.content.quantidade
+      };
+      this.$emit("input", retorno);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
